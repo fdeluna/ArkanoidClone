94c6b55a3e6ded277de7bdc445f4f18dfaf4b58f
fdeluna <fernando.deluna.romero@gmail.com>
2019-02-28 19:39:25 +0100

    Unity physics works better

--- Assets/Scripts/Controller/BallController.cs
+++ Assets/Scripts/Controller/BallController.cs
@@ -19,8 +19,6 @@ public class BallController : MonoBehaviour
     private Rigidbody2D _rigidBody;
     private Collider2D _collider2D;
     private bool _brickHitted = false;
-    private int _layerMask;
-    private Vector3 _contactPoint = Vector3.zero;
 
 
     public delegate void BallDestroyed();
@@ -31,7 +29,6 @@ public class BallController : MonoBehaviour
         _rigidBody = GetComponent<Rigidbody2D>();
         _collider2D = GetComponent<Collider2D>();
         _paddle = FindObjectOfType<PaddleController>();
-        _layerMask = (1 << LayerMask.NameToLayer("Brick")) | (1 << LayerMask.NameToLayer("Walls")) | (1 << LayerMask.NameToLayer("Player"));
     }
 
     private void OnEnable()
@@ -46,7 +43,6 @@ public class BallController : MonoBehaviour
         {
             Vector3 position = !Magnet ? new Vector3(_paddle.transform.position.x, _paddle.transform.position.y + 0.5f) : new Vector3(_paddle.transform.position.x - _contactPointX, _paddle.transform.position.y + 0.5f);
             _rigidBody.position = position;
-
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 IsLaunched = true;
@@ -54,48 +50,50 @@ public class BallController : MonoBehaviour
         }
         else
         {
-            MoveBall();
+            _rigidBody.velocity = _direction.normalized * speed;
         }
     }
 
-    private void MoveBall()
+    private void LateUpdate()
     {
-        Vector2 nextPosition = _rigidBody.position + (_direction.ToVector2() * speed) * Time.fixedDeltaTime;
-        Collider2D collider = Physics2D.OverlapBox(nextPosition, _collider2D.bounds.size.ToVector2() * 0.5f, 0, _layerMask);
-        if (collider != null)
-        {
-            _brickHitted = true;
-            _rigidBody.position = _rigidBody.position + (_direction.ToVector2() * collider.Distance(_collider2D).distance);
-            Vector2 newDirection = Vector2.Reflect(_direction, collider.Distance(_collider2D).normal).normalized;
-            speed = Mathf.Clamp(speed + 0.1f, 4, 8);
+        _brickHitted = false;
+    }
 
-            switch (collider.tag)
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (IsLaunched && !_brickHitted)
+        {
+            ContactPoint2D contactPoint = collision.contacts[0];
+            Vector3 newDirection = Vector3.Reflect(_direction, contactPoint.normal);
+            _brickHitted = true;
+            speed = Mathf.Clamp(speed + 0.1f, 3.5f, 8);
+
+            switch (collision.collider.tag)
             {
                 case "Player":
-                    RaycastHit2D hit = Physics2D.Raycast(_rigidBody.position, _direction);
                     if (Magnet)
                     {
                         IsLaunched = false;
-                        _contactPointX = _paddle.transform.position.x - hit.point.x;
+                        _contactPointX = _paddle.transform.position.x - contactPoint.point.x;
                     }
                     else
                     {
-                        Vector3 center = collider.bounds.center;
-                        newDirection.x += center.x > hit.point.x ? -deviation : deviation;
+                        Vector3 center = collision.collider.bounds.center;
+                        newDirection.x += center.x > contactPoint.point.x ? -deviation : deviation;
                     }
                     break;
                 case "Brick":
-                    Brick brick = collider.GetComponent<Brick>();
+                    Brick brick = collision.collider.GetComponent<Brick>();
                     brick.Hit();
                     if (IsSuperBall)
                     {
+                        _brickHitted = false;
                         newDirection = _direction;
                     }
                     break;
             }
             _direction = newDirection;
         }
-        _rigidBody.MovePosition(_rigidBody.position + (_direction.ToVector2().normalized * speed) * Time.fixedDeltaTime);
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
